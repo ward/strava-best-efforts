f6723947ef393e5c62bfbf2008a479939f6abd80
Ward Muylaert <ward.muylaert@gmail.com>
2015-01-21 19:13:40 +0100

    SQLite schema for the db

---
+++ create_tables.sql
@@ -0,0 +1,26 @@
+CREATE TABLE "activity" (
+    "id" INTEGER PRIMARY KEY NOT NULL,
+    "resource_state" INTEGER,
+    "external_id" TEXT,
+    "athlete_id" INTEGER,
+    "name" TEXT,
+    "description" TEXT,
+    "distance" REAL,
+    "moving_time" INTEGER,
+    "elapsed_time" INTEGER,
+    "total_elevation_gain" REAL,
+    "type" TEXT,
+    "start_date" INTEGER,
+    "trainer" INTEGER,
+    "commute" INTEGER,
+    "manual" INTEGER,
+    "private" INTEGER,
+    "flagged" INTEGER
+);
+CREATE TABLE "best_effort" (
+    "distance" INTEGER NOT NULL,
+    "moving_time" INTEGER NOT NULL,
+    "elapsed_time" INTEGER NOT NULL,
+    "activity_id" INTEGER NOT NULL,
+    PRIMARY KEY ("distance", "activity_id")
+);
