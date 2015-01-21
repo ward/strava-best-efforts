514ddeb695391c470d36fcd56b66b357ce6801a0
Ward Muylaert <ward.muylaert@gmail.com>
2015-01-21 19:13:59 +0100

    Some code! Fetches runs from strava, saves locally

---
+++ main.rb
@@ -0,0 +1,87 @@
+# They really should've documented this part
+require 'strava/api/v3'
+require 'yaml'
+require 'sqlite3'
+# For parse
+require 'time'
+
+config = YAML.load_file('config.yml')
+
+@client = Strava::Api::V3::Client.new(access_token: config['access_token'])
+@db = SQLite3::Database.new "strava.db"
+
+# Gets a list of summaries for all the runs. Since we need the best
+# efforts, this will not do.
+def get_all_runs
+  page = 1
+  runs = []
+
+  begin
+    acts = @client.list_athlete_activities(per_page: 50, page: page)
+    only_runs = acts.reject { |act| act['type'] != 'Run' }
+    runs = runs + only_runs
+    page = page + 1
+  end until acts.length == 0
+
+  return runs
+end
+
+# Monkey patch true and false. SQLite does not have booleans
+class FalseClass; def to_i; 0 end end
+class TrueClass; def to_i; 1 end end
+
+
+# Takes as input a JSON run activity as retrieved from a specific API call
+def run_to_sql run
+  # First build hash of what we will put in the db
+  keyval = {}
+
+  keyval['id'] = run['id']
+  keyval['resource_state'] = run['resource_state']
+  keyval['external_id'] = run['external_id']
+  keyval['athlete_id'] = run['athlete']['id']
+  keyval['name'] = run['name']
+  keyval['description'] = run['description']
+  keyval['distance'] = run['distance']
+  keyval['moving_time'] = run['moving_time']
+  keyval['elapsed_time'] = run['elapsed_time']
+  keyval['total_elevation_gain'] = run['total_elevation_gain']
+  keyval['type'] = run['type']
+  keyval['start_date'] = Time.parse(run['start_date']).to_i
+  keyval['trainer'] = run['trainer'].to_i
+  keyval['commute'] = run['commute'].to_i
+  keyval['manual'] = run['manual'].to_i
+  keyval['private'] = run['private'].to_i
+  keyval['flagged'] = run['flagged'].to_i
+
+  begin
+    # TODO: Can we assume .keys and .values will always return in same order?
+    @db.execute("INSERT INTO activity (#{keyval.keys.join(', ')})
+              VALUES (#{('?,' * keyval.length)[0..-2]})",
+              keyval.values)
+
+    # Now the best efforts
+    run['best_efforts'].each do |effort|
+      @db.execute("INSERT INTO best_effort (distance, moving_time, elapsed_time, activity_id)
+                VALUES (?,?,?,?)",
+                [effort['distance'],
+                 effort['moving_time'],
+                 effort['elapsed_time'],
+                 effort['activity']['id']])
+    end
+  rescue SQLite3::ConstraintException => e
+    puts e
+    puts "Error for #{run['id']}"
+  end
+end
+
+activities = get_all_runs()
+
+puts "#{activities.length} activities"
+activities.each do |activity|
+  puts "Activity #{activity['id']}... "
+  run_to_sql(@client.retrieve_an_activity(activity['id']))
+  puts "handled."
+end
+puts "finished"
+
